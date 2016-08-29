41d2d8e408097ecce19cec6b2020bfe97e2cc73d
hunter007 <wentao79@gmail.com>
2016-08-29 14:23:29 +0800

    修复过期时间为-1

--- django_zqiniu/storage.py
+++ django_zqiniu/storage.py
@@ -1,12 +1,11 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import os
-
+from six.moves.urllib.parse import urljoin
 from django.conf import settings
 from django.utils.deconstruct import deconstructible
-from django.core.files import File
 from django.core.files.storage import Storage
 
-from qiniu import Auth, BucketManager, put_file
+from qiniu import Auth, BucketManager, put_data
 
 
 @deconstructible
@@ -19,17 +18,25 @@ class QiniuStorage(Storage):
         if not bucket_config:
             bucket_config = settings.QINIU_SETTINGS.get('DEFAULT_BUCKET')
         assert bucket_config in settings.QINIU_SETTINGS['BUCKET_CONFIGS']
-        assert 'BUCKET_URL' in settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config]
-        assert 'BUCKET_NAME' in settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config]
+        assert 'BUCKET_URL' in settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config]
+        assert 'BUCKET_NAME' in settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config]
 
         self.access_key = settings.QINIU_SETTINGS['ACCESS_KEY']
         self.secret_key = settings.QINIU_SETTINGS['SECRET_KEY']
-        self.bucket_name = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config]['BUCKET_NAME']
-        self.bucket_url = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config]['BUCKET_URL']
-        self.bind_url = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config].get('BIND_URL', '')
-        self.prefix = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config].get('PREFIX', '')
-        self.is_private_bucket = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config].get('IS_PRIVATE', False)
-        self.expires_time = settings.QINIU_SETTINGS['BUCKET_CONFIGS'][bucket_config].get('EXPIRES', 3600)
+        self.bucket_name = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config]['BUCKET_NAME']
+        self.bucket_url = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config]['BUCKET_URL']
+        self.bind_url = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config].get('BIND_URL', '')
+        self.prefix = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config].get('PREFIX', '')
+        self.is_private_bucket = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config].get('IS_PRIVATE', False)
+        self.expires_time = settings.QINIU_SETTINGS[
+            'BUCKET_CONFIGS'][bucket_config].get('EXPIRES', 3600)
 
         self.__auth = Auth(self.access_key, self.secret_key)
         self.__bucket_manager = BucketManager(self.__auth)
@@ -39,7 +46,8 @@ class QiniuStorage(Storage):
             self.__bucket_manager.delete(self.bucket_name, self.prefix + name)
 
     def exists(self, name):
-        ret, info = self.__bucket_manager.stat(self.bucket_name, self.prefix + name)
+        ret, info = self.__bucket_manager.stat(
+            self.bucket_name, self.prefix + name)
         return 'hash' in ret
 
     def path(self, name):
@@ -48,11 +56,13 @@ class QiniuStorage(Storage):
     def listdir(self, prefix):
         if not prefix:
             prefix = self.prefix
-        ret, eof, info = self.__bucket_manager.list(self.bucket_name, prefix=prefix)
+        ret, eof, info = self.__bucket_manager.list(
+            self.bucket_name, prefix=prefix)
         return ret
 
     def size(self, name):
-        ret, info = self.__bucket_manager.stat(self.bucket_name, self.prefix + name)
+        ret, info = self.__bucket_manager.stat(
+            self.bucket_name, self.prefix + name)
         return ret['fsize']
 
     def url(self, name):
@@ -69,8 +79,9 @@ class QiniuStorage(Storage):
 
     def save(self, name, content, max_length=None):
         file_name = os.path.basename(name)
-        token = self.__auth.upload_token(self.bucket_name, self.path(file_name), -1)
-        ret, info = put_file(token, self.path(file_name), name)
+        token = self.__auth.upload_token(
+            self.bucket_name, self.path(file_name), 3600)
+        ret, info = put_data(token, self.path(file_name), content)
         return self.path(file_name)
 
     def __eq__(self, other):
